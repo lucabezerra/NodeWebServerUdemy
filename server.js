c56f5f2f4c61874f019415e30911e589c0bc3559
Luca Bezerra <luca@pool.ai>
2017-05-04 12:30:04 -0300

    Adding projects page.

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get("/about", (req, res) => {
 	});
 });
 
+app.get("/projects", (req, res) => {
+	res.render("projects.hbs", {
+		pageTitle: "My Projects",
+	});
+});
+
 app.listen(port, () => {
 	console.log("Server is up and listening to port " + port + ".");
 });
